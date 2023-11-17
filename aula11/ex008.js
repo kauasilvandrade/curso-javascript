96fecc9130cd2e6f3e15c079ea8a39948fe772f4
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2023-11-16 21:55:10 -0300

    terminado ex de condições

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+let vel = 70
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) { // CONDIÇÃO SIMPLES
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+let pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+
+if (pais != 'Brasil') { // CONDIÇÃO COMPOSTA
+     console.log('Você é Estrangeiro!')
+} else {
+    console.log('Você é Brasileiro!')
+}
