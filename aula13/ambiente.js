160998bd5120cd151d050042afb0fa053b98cc24
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2023-12-03 13:34:01 -0300

    ex terminado

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+let contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador ++ 
+} while (contador <= 6)
+
+/* let contador = 1
+while (contador <= 6) {
+    console.log(`Passo ${contador}`)
+    contador ++ // contador = contador + 1
+} */
+
+/* console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */
