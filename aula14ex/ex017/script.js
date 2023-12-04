96d67caa70b2e2aede3caa2aefface41c449b589
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2023-12-04 19:37:28 -0300

    ex terminado

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,23 @@
+function carregar() {
+    let msg = document.getElementById('msg');
+    let img = document.getElementById('imagem');
+    let data = new Date()
+    let hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA
+        img.src = "img/manha.png"
+        document.body.style.background = "#e2cd9f"
+    } else if (hora >= 12 && hora <= 18) {
+        // BOA TARDE
+        img.src = "img/tarde.png"
+        document.body.style.background = "#b9846f"
+    } else {
+        // BOA NOITE
+        img.src = "img/noite.png"
+        document.body.style.background = "#515154"
+    }
+
+}
